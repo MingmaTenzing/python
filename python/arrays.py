51bed2572986388b12f41d14b5efbfe4176bf81e
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-08-28 12:34:33 +0800

    arrays

---
+++ python/arrays.py
@@ -0,0 +1,11 @@
+# Python doesn't support arrays, you need to import the numpy library 
+# however, you can use lists as array 
+
+
+cars = ["ford", "volvo", "bmw"]
+
+cars[0] = "toyota"
+cars.append("mazda")
+cars.pop(1)
+cars.remove("toyota")
+print(cars)
