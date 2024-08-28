2376b248613525ba378cdf8168a9de2edc2f1ca2
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-08-28 10:07:53 +0800

    dictonaries

---
+++ python/dictonaries.py
@@ -0,0 +1,9 @@
+thisdict = {
+    "brand": "Toyota",
+    "model": "Aurion",
+    "year": 2007
+}
+
+
+for x in thisdict:
+    print(thisdict[x])
---
+++ python/lists/find.py
@@ -0,0 +1,6 @@
+thislist = ["apple", "banana", "cherry"]
+
+for i in thislist:
+    if(i == "cherry"):
+        print(i)
+        
