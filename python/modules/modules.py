ab2162fdd4eb7a66c6fa3972598d3969d7066903
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-08-29 23:54:54 +0800

    modules

---
+++ python/modules/modules.py
@@ -0,0 +1,12 @@
+import sum
+import person as mx 
+import platform
+
+sum.sum(1,2)
+
+human = mx.person1["age"]
+x = dir(sum)
+print(x)
+print(human)
+print(platform.release())
+
---
+++ python/modules/person.py
@@ -0,0 +1,6 @@
+person1= {
+    "name":"John",
+    "age":30,
+    "country": "Nepal"
+}
+
